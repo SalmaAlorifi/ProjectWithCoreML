082ab97c8b7dbc9ccaf30801c767da8175415fd4
salmadwv101 <sfalorifi22@twq.idserve.net>
2023-07-03 21:36:30 +0300

    Initial Commit

---
+++ ProjectWithCoreML/ProjectWithCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectWithCoreMLApp.swift
+//  ProjectWithCoreML
+//
+//  Created by salma alorifi on 15/12/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ProjectWithCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
