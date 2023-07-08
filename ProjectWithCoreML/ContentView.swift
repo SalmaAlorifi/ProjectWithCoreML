fc971393fd84b9c1c8912ccb8acc274fed972512
salmadwv101 <sfalorifi22@twq.idserve.net>
2023-07-08 17:48:08 +0300

    full code

--- ProjectWithCoreML/ContentView.swift
+++ ProjectWithCoreML/ContentView.swift
@@ -6,16 +6,86 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
+    
+    let images = ["car","cat","cloud","orange","person","strawberry"]
+    
+    @State private var currentIndex: Int = 0
+    @State private var classificationResult: String = ""
+    
+    let model: MobileNetV2 = {
+        do {
+            let config = MLModelConfiguration()
+            return try MobileNetV2(configuration: config)
+        } catch {
+            print(error)
+            fatalError("Couldn't create MobileNetV2")
+        }
+    }()
+    
+    private func classifyImage() {
+        guard let image = UIImage(named: images[currentIndex]),
+              let ciImage = CIImage(image: image) else {
+            return
+        }
+        
+        let mlModel = try! VNCoreMLModel(for: model.model)
+        let request = VNCoreMLRequest(model: mlModel) { request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                  let topResult = results.first else {
+                self.classificationResult = "Error classifying the image."
+                return
+            }
+            
+            self.classificationResult = "Classified as: \(topResult.identifier)"
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+            classificationResult = "Error classifying the image."
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Image Classifier")
+                .padding()
+                .font(.largeTitle)
+            
+            Image(images[currentIndex])
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+                .padding()
+            
+            Button("Next Image") {
+                currentIndex = (currentIndex + 1) % images.count
+            }
+            .padding()
+            .foregroundColor(.white)
+            .frame(width: 200, height: 50)
+            .background(Color.blue)
+            .cornerRadius(10)
+            Button("Classify") {
+                
+                classifyImage()
+                
+            }
+            .padding()
+            .foregroundColor(.white)
+            .frame(width: 200, height: 50)
+            .background(Color.green)
+            .cornerRadius(10)
+            
+            Text(classificationResult)
+                .padding()
         }
-        .padding()
     }
 }
 
